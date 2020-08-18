cf02f62f3c031bf58069b14222fa5a5419ccd067
naiem <naiem4288@gamil.com>
2020-08-18 21:39:12 +0600

    creat hard rock project

--- app.js
+++ app.js
@@ -1,27 +1,62 @@
 
-  const searchBox = document.querySelector('#search_box');
-
+const searchBox = document.querySelector('#search_box');
 const searchBtn = document.querySelector('#search_btn');
+
+
 searchBtn.addEventListener('click', function () {
-  
+  // ===API FETCH FOR LYRICS INFO===
   fetch(`https://api.lyrics.ovh/suggest/${searchBox.value}`)
     .then(res => res.json())
-    .then(data => getInfo(data.data))
-  
-  function getInfo(data) {
-   let dataInfo =  data.map(title => title.title);
-    console.log(dataInfo)
-  }
+    .then(data => {
+
+      let parentElement = document.querySelector('.search-result');
+      parentElement.innerHTML = ''
+
+      for (let i = 0; i < 10; i++) {
+        lyricsInfo = data.data[i];
+
+        let lyricsTitle = lyricsInfo.title;
+        let albumTitle  = lyricsInfo.album.title;
+        let artistName = lyricsInfo.artist.name;
+
         
-
-  
-
-  
+        // ===DISPLAY DYNAMIC LYRIC INFORMATION ===
+        parentElement.innerHTML += `<div class="single-result row align-items-center my-3 p-3 ">
+        <div class=" col-8 col-md-9">
+            <h3 class="lyrics-name text-uppercase">${lyricsTitle}</h3>
+            <p class=" lead"><span class="font ">Album by :</span> <span class="album_name font-italic">${albumTitle}</span></p>
+            <p class="lead artist"> <span class="font">Artist Name :</span> <span class="artist_name font-italic">${artistName}</span> </p>
+        </div>
+        <div class=" col-4 col-md-3 text-md-right text-center">
+            <button class="btn btn-success get_lyrics" onclick="getLyrics('${lyricsTitle}','${artistName}')">Get Lyrics</button>
+        </div>
+    </div>`     
+        
+        
+      }
+      
+    })
 })
 
- 
-// function getResult(artist, title) {
-//   fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
-//     .then(response => response.json())
-//     .then(data => console.log(data));
-// }
+
+// === LYRICS GENERATOR FUNCTION===
+function getLyrics(title, artist) {
+    fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    .then(res => res.json())
+      .then(data => {
+
+        const songLyrics = data.lyrics;
+        if (songLyrics !== undefined) {
+          let singleLyrics = document.querySelector('.single-lyrics')
+          singleLyrics.innerHTML = '';
+          singleLyrics.innerHTML += `<h2 class="text-success mb-4 font-weight-bold">${title} - ${artist}</h2>
+        <pre class="lyrics text-white"> ${songLyrics}</pre>`
+        } else {
+          alert('No Lyrics Available')
+        }
+
+        
+      });
+}
+
+// =============The End=================
